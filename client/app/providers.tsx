edd604c0520e6d727b098c0dfa1328f55adb7f3a
GochiStuff <ncfdmytime@gmail.com>
2025-09-22 02:04:16 +0530

    all delete .

--- client/app/providers.tsx
+++
@@ -1,8 +0,0 @@
-"use client"
-
-import { WebRTCProvider } from "@/context/WebRTCContext"
-import React from "react"
-
-export default function Providers({children } : { children : React.ReactNode}){
-    return <WebRTCProvider>{children}</WebRTCProvider>;
-}
--- server/src/config/index.js
+++
@@ -1,9 +0,0 @@
-
-import dotenv from 'dotenv';
-dotenv.config();
-
-export const DB_URI = process.env.DB_URI;
-export const PORT = process.env.PORT || 5500;
-export const CORS_ORIGIN = process.env.CORS_ORIGIN ? process.env.CORS_ORIGIN.split(',') : ["http://localhost:3000"];
-export const NODE_ENV = process.env.NODE_ENV !== 'production';
-export const ENABLE_ANALYTICS = process.env.ENABLE_ANALYTICS === 'true';
